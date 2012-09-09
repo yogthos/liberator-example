5df7d0844b0af43280811315f66db9016effac61
Yogthos <dmitri.sotnikov@gmail.com>
2012-09-09 13:40:50 -0400

    formatting

--- resources/public/site.js
+++ resources/public/site.js
@@ -1,25 +1,25 @@
 function getUserInfo() {
-		$.get("/user-info", 
-		      function(data) {
-		      	$("#message").text("Welcome " + data.firstname + " " + data.lastname);});
-	}
+	$.get("/user-info", 
+	      function(data) {
+	      	$("#message").text("Welcome " + data.firstname + " " + data.lastname);});
+}
 	
-	function login() {
-		$("#message").text("sending login request");
-		$.post("/login", 
-		       {user: $("#user").val(), pass: $("#pass").val()}, 
-	    	   function(){	
-	    	   		window.location.reload(true);	    	   		    	   	    	    	       	    	       
-	    	   },
-	    	   "json")
-	     .error( function(xhr, textStatus, errorThrown) {       			 
-       			 $("#message").text(textStatus + ": " + xhr.responseText);
-    	 });
-	}
-	
-	function logout() {
-		$.post("/logout", 
-		       function() {
-					window.location.reload(true);
-				});					  	
-	}
+function login() {
+	$("#message").text("sending login request");
+	$.post("/login", 
+	       {user: $("#user").val(), pass: $("#pass").val()}, 
+    	   function(){	
+    	   		window.location.reload(true);	    	   		    	   	    	    	       	    	       
+    	   },
+    	   "json")
+     .error( function(xhr, textStatus, errorThrown) {       			 
+      			 $("#message").text(textStatus + ": " + xhr.responseText);
+   	 });
+}
+
+function logout() {
+	$.post("/logout", 
+	       function() {
+				window.location.reload(true);
+			});					  	
+}
